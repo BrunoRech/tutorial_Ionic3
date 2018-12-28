140cac259b6318726b0f1b44507ed362ed7bd265
brunoRech <brunogrech@hotmail.com>
2018-12-28 17:16:06 -0200

    Insercao de refresh e loading no feed

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { MovieProvider } from '../../providers/movie/movie';
 
 /**
@@ -16,9 +16,10 @@ import { MovieProvider } from '../../providers/movie/movie';
   providers:[MovieProvider]
 })
 export class FeedPage {/*Manda a classe feed (p/ exemplo na tabs.ts) */
-  
+  public loader;
   public movieList = new Array<any>();
-
+  public refresher;
+  public isRefreshing: boolean = false;
   public feed = {//JSON
     title: "Erik Von-Strawssen",
     date:"December 5, 1655",
@@ -31,18 +32,45 @@ export class FeedPage {/*Manda a classe feed (p/ exemplo na tabs.ts) */
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
-     private movieProv: MovieProvider) {//injetando o provider aqui
+     private movieProv: MovieProvider,
+     public loadingCtrl: LoadingController) {//injetando o provider aqui
   }
 
-  ionViewDidLoad() {
+  doRefresh(refresher){
+    this.refresher = refresher;
+    this.isRefreshing = true;
+    this.carregarFilmes();
+  }
+
+  openLoading() {//animacao do loading
+    this.loader = this.loadingCtrl.create({
+      content: "Carregando filmes...",
+    });
+    this.loader.present();
+  }
+  closeLoading(){
+    this.loader.dismiss();
+  }
+  carregarFilmes(){
+     this.openLoading();
     this.movieProv.getPopularMovies().subscribe(data=>{
       console.log(data);
       this.movieList = (data as any).results;
+      this.closeLoading();
+      if(this.isRefreshing){
+        this.refresher.complete();
+        this.isRefreshing = false;
+      }
     },
     error => {
       console.log(error);
+      this.closeLoading();
     }
     )
+  
+  }
+  ionViewDidEnter() {//toda vez que o usuario entrar na pag
+    this.carregarFilmes();
   }
 
 }
